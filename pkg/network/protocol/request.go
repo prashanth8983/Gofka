b0bb7316e67515abb5e9ed6a96d12f7e574d141a
Prashanth Kumar <prashanth8983@gmail.com>
2025-10-04 23:34:33 -0400

    Add network protocol handlers and metadata management

---
+++ pkg/network/protocol/request.go
@@ -0,0 +1,11 @@
+package protocol
+
+// Request represents a Gofka network request.
+type Request struct {
+	// TODO: Define common request fields (e.g., API key, API version, correlation ID)
+}
+
+// Response represents a Gofka network response.
+type Response struct {
+	// TODO: Define common response fields (e.g., correlation ID)
+}
